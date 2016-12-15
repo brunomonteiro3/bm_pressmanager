6b5606c05d1a1afd297eb9276242ed1a734bca71
Bruno Monteiro <brunomonteiro3@gmail.com>
2016-12-14 22:42:49 -0200

    Basic structure.

--- index.php
+++ index.php
@@ -1,1 +1,38 @@
-index.php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/assets/css/default.css" />
+	<link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/assets/css/bootstrap.css" />
+</head>
+<body>
+	<nav class="navbar navbar-default navbar-fixed-top">
+		<div class="container">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+			
+				<a class="navbar-brand" href="#">BM Press Manager</a>
+			</div>
+
+
+			<form class="navbar-form navbar-right">
+				<div class="form-group">
+					<input type="text" class="form-control" placeholder="Search">
+				</div>
+				
+				<button type="submit" class="btn btn-default">Submit</button>
+			</form>
+		</div>
+	</nav>
+
+	<div class="container">
+		<h1>ok</h1>
+	</div>
+</body>
+</html>
